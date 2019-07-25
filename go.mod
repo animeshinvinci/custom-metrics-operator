722b4dfb41daa739c14324020402357cd476fb0a
huanggze <loganhuang@yunify.com>
2019-07-25 21:52:22 +0800

    init the project, add framework
    Signed-off-by: huanggze <loganhuang@yunify.com>

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/huanggze/custom-metrics-operator
+
+go 1.11
+
+require (
+	github.com/coreos/prometheus-operator v0.27.0
+	github.com/go-openapi/spec v0.19.2 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/googleapis/gnostic v0.3.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.0.0-20190222213804-5cb15d344471 // indirect
+	k8s.io/apimachinery v0.0.0-20190221213512-86fb29eff628
+	k8s.io/client-go v10.0.0+incompatible
+	k8s.io/klog v0.3.3
+	k8s.io/kube-openapi v0.0.0-20190722073852-5e22f3d471e6 // indirect
+	k8s.io/sample-controller v0.0.0-20190325192909-fcd83ae9b375
+	sigs.k8s.io/yaml v1.1.0 // indirect
+)
---
+++ pkg/operator/operator.go
@@ -0,0 +1,205 @@
+package operator
+
+import (
+	"fmt"
+	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	informers "github.com/coreos/prometheus-operator/pkg/client/informers/externalversions/monitoring/v1"
+	listers "github.com/coreos/prometheus-operator/pkg/client/listers/monitoring/v1"
+	clientset "github.com/coreos/prometheus-operator/pkg/client/versioned"
+	"k8s.io/apimachinery/pkg/api/errors"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"time"
+
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
+)
+
+// Controller is the controller implementation for ServiceMonitor resources
+type Controller struct {
+	// sampleclientset is a clientset for our own API group
+	monitclientset clientset.Interface
+
+	smonsLister listers.ServiceMonitorLister
+	smonsSynced cache.InformerSynced
+
+	config Config
+	// workqueue is a rate limited work queue. This is used to queue work to be
+	// processed instead of performing it as soon as a change happens. This
+	// means we can ensure we only process a fixed amount of resources at a
+	// time, and makes it easy to ensure we are never processing the same item
+	// simultaneously in two different workers.
+	workqueue workqueue.RateLimitingInterface
+}
+
+// Config defines configuration parameters for the Operator.
+type Config struct {
+}
+
+// NewController returns a new custom metrics operator
+func NewController(
+	monitclientset clientset.Interface,
+	smonInformer informers.ServiceMonitorInformer,
+	conf Config) *Controller {
+
+	controller := &Controller{
+		monitclientset: monitclientset,
+		smonsLister:    smonInformer.Lister(),
+		smonsSynced:    smonInformer.Informer().HasSynced,
+		config:         conf,
+		workqueue:      workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "ServiceMonitors"),
+	}
+
+	klog.Info("Setting up event handlers")
+	// Set up an event handler for when ServiceMonitor resources change
+	smonInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: controller.enqueueServiceMonitor,
+		UpdateFunc: func(old, new interface{}) {
+			newSmon := new.(*monitoringv1.ServiceMonitor)
+			oldSmon := old.(*monitoringv1.ServiceMonitor)
+			if newSmon.ResourceVersion == oldSmon.ResourceVersion {
+				// Periodic resync will send update events for all known ServiceMonitors.
+				// Two different versions of the same ServiceMonitor will always have different RVs.
+				return
+			}
+			controller.enqueueServiceMonitor(new)
+		},
+		DeleteFunc: controller.enqueueServiceMonitor,
+	})
+
+	return controller
+}
+
+// Run will set up the event handlers for types we are interested in, as well
+// as syncing informer caches and starting workers. It will block until stopCh
+// is closed, at which point it will shutdown the workqueue and wait for
+// workers to finish processing their current work items.
+func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
+	defer utilruntime.HandleCrash()
+	defer c.workqueue.ShutDown()
+
+	// Start the informer factories to begin populating the informer caches
+	klog.Info("Starting Custom Metrics Operator")
+
+	// Wait for the caches to be synced before starting workers
+	klog.Info("Waiting for informer caches to sync")
+	if ok := cache.WaitForCacheSync(stopCh, c.smonsSynced); !ok {
+		return fmt.Errorf("failed to wait for caches to sync")
+	}
+
+	klog.Info("Starting workers")
+	// Launch two workers to process ServiceMonitor resources
+	for i := 0; i < threadiness; i++ {
+		go wait.Until(c.runWorker, time.Second, stopCh)
+	}
+
+	klog.Info("Started workers")
+	<-stopCh
+	klog.Info("Shutting down workers")
+
+	return nil
+}
+
+// runWorker is a long-running function that will continually call the
+// processNextWorkItem function in order to read and process a message on the
+// workqueue.
+func (c *Controller) runWorker() {
+	for c.processNextWorkItem() {
+	}
+}
+
+// processNextWorkItem will read a single work item off the workqueue and
+// attempt to process it, by calling the syncHandler.
+func (c *Controller) processNextWorkItem() bool {
+	obj, shutdown := c.workqueue.Get()
+
+	if shutdown {
+		return false
+	}
+
+	// We wrap this block in a func so we can defer c.workqueue.Done.
+	err := func(obj interface{}) error {
+		// We call Done here so the workqueue knows we have finished
+		// processing this item. We also must remember to call Forget if we
+		// do not want this work item being re-queued. For example, we do
+		// not call Forget if a transient error occurs, instead the item is
+		// put back on the workqueue and attempted again after a back-off
+		// period.
+		defer c.workqueue.Done(obj)
+		var key string
+		var ok bool
+		// We expect strings to come off the workqueue. These are of the
+		// form namespace/name. We do this as the delayed nature of the
+		// workqueue means the items in the informer cache may actually be
+		// more up to date that when the item was initially put onto the
+		// workqueue.
+		if key, ok = obj.(string); !ok {
+			// As the item in the workqueue is actually invalid, we call
+			// Forget here else we'd go into a loop of attempting to
+			// process a work item that is invalid.
+			c.workqueue.Forget(obj)
+			utilruntime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
+			return nil
+		}
+		// Run the syncHandler, passing it the namespace/name string of the
+		// ServiceMonitor resource to be synced.
+		if err := c.syncHandler(key); err != nil {
+			// Put the item back on the workqueue to handle any transient errors.
+			c.workqueue.AddRateLimited(key)
+			return fmt.Errorf("error syncing '%s': %s, requeuing", key, err.Error())
+		}
+		// Finally, if no error occurs we Forget this item so it does not
+		// get queued again until another change happens.
+		c.workqueue.Forget(obj)
+		klog.Infof("Successfully synced '%s'", key)
+		return nil
+	}(obj)
+
+	if err != nil {
+		utilruntime.HandleError(err)
+		return true
+	}
+
+	return true
+}
+
+// syncHandler compares the actual state with the desired, and attempts to
+// converge the two. It then updates the Status block of the ServiceMonitor resource
+// with the current status of the resource.
+func (c *Controller) syncHandler(key string) error {
+	// Convert the namespace/name string into a distinct namespace and name
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		return nil
+	}
+
+	// Get the ServiceMonitor resource with this namespace/name
+	smon, err := c.smonsLister.ServiceMonitors(namespace).Get(name)
+	// The ServiceMonitor resource may no longer exist, in which case we remove it from Prometheus.
+	if errors.IsNotFound(err) {
+		// TODO: add business logic here
+	}
+	if err != nil {
+		return err
+	}
+
+	// TODO: add business logic here
+	klog.Infof("ServiceMonitor: %s/%s", smon.Namespace, smon.Name)
+
+	return nil
+}
+
+// enqueueServiceMonitor takes a ServiceMonitor resource and converts it into a namespace/name
+// string which is then put onto the work queue. This method should *not* be
+// passed resources of any type other than ServiceMonitor.
+func (c *Controller) enqueueServiceMonitor(obj interface{}) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		utilruntime.HandleError(err)
+		return
+	}
+	c.workqueue.Add(key)
+}
